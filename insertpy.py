7d46e3fd4c82435863f293280be54b5812bae6c7
Luca <lucastoian@outlook.com>
2024-04-19 07:37:16 +0200

    go

--- insertpy.py
+++ insertpy.py
@@ -2,15 +2,18 @@ import pandas as pd
 xl = pd.ExcelFile('PokemonTCG_EmptyCheckList_v9.3.xlsm')
 
 
-df = xl.parse('151')
+df = xl.parse('Gym Heroes')
 
+type_zero_index = True
+expansion_id = 36
+base_url = "https://tcgmarket.s3.eu-west-3.amazonaws.com/pokemon/singles/gym-heroes/"
 
 # Select the required columns
 required_columns = ['Name', 'Set Name', 'Number', 'Type', 'HP', 'Stage', 'Rarity']
-base_url = "https://tcgmarket.s3.eu-west-3.amazonaws.com/pokemon/singles/151/SV3pt5_EN_"
+
 
 # Define a function to create SQL insert statements for the dataframe rows
-def create_insert_statements(df, base_url, expansion_id=86):
+def create_insert_statements(df, base_url,type_zero_index, expansion_id):
     insert_statements = []
 
     for index, row in df.iterrows():
@@ -18,7 +21,18 @@ def create_insert_statements(df, base_url, expansion_id=86):
         name = str(row['Name']).replace("'", "''")
         set_name = str(row['Set Name'])
         # Construct the image URL by appending the iteration number to the base URL
-        image_url = f"{base_url}{index + 1}.png"
+        if(type_zero_index):
+            idx=''
+            if((index+1) < 10):
+                idx = '00' + str((index + 1))
+            if((index+1) > 10 and (index+1) < 100):
+                idx = '0' + str((index + 1))
+            if((index+1) > 100):
+                idx = (index + 1)
+            image_url = f"{base_url}{idx}.jpg"
+        else:
+            image_url = f"{base_url}{index + 1}.png"
+
 
         # Format the INSERT statement for SQL
         insert_statement = f"INSERT INTO tcg.Card (name, number, image, rarity, stage, type, hp, set_name, expansion_id, description, price, quantity) VALUES ('{name}', '{row['Number']}', '{image_url}', '{row['Rarity']}', '{row['Stage']}', '{row['Type']}', {row['HP'] if pd.notna(row['HP']) else 'NULL'}, '{set_name}', {expansion_id}, NULL, NULL, NULL);"
@@ -28,7 +42,7 @@ def create_insert_statements(df, base_url, expansion_id=86):
 
 # Check if all required columns are in the DataFrame and create the insert statements
 
-insert_statements = create_insert_statements(df[required_columns], base_url)
+insert_statements = create_insert_statements(df[required_columns], base_url, type_zero_index, expansion_id)
 
 
 
